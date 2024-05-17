9fabf3ff1a661654cef315d9aa34f1f3fd864751
Harvey Bernardo <80321695+harveydB@users.noreply.github.com>
2024-05-17 17:18:04 -0400

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -4,8 +4,6 @@ from selenium.webdriver.chrome.options import Options
 import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from bs4 import BeautifulSoup
-
 
 
 chrome_options = Options()
